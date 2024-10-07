9959ea35048d0ea6e8291dc40903121a8957d0cf
Michael Sucker <michael_sucker@t-online.de>
2024-10-07 14:42:09 +0200

    Added implementation of 'run_experiment()'.

--- experiments/nn_training/run_experiment.py
+++ experiments/nn_training/run_experiment.py
@@ -1,3 +1,15 @@
+from experiments.nn_training.evaluation import evaluate_nn
+from experiments.nn_training.plotting import create_plot
+
 
 def run_experiment(path: str) -> None:
-    pass
+    """Run the experiment on training a neural network.
+
+    :param path: path where the trained model is stored, and where the data/plot get saved into
+    """
+
+    # Evaluate model
+    evaluate_nn(loading_path=path)
+
+    # Create plot
+    create_plot(loading_path=path)
