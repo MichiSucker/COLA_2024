228c2ea092c0fe8f62ae7c9dd45d2a75d54cf76d
Michael Sucker <michael_sucker@t-online.de>
2024-10-07 13:58:10 +0200

    Added functions 'train_model()', 'tensor_to_nn()' and 'nn_to_tensor()'.

--- experiments/nn_training/neural_network.py
+++ experiments/nn_training/neural_network.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
+from typing import Tuple, Callable
 
 
 def polynomial_features(x: torch.Tensor, degree: int) -> torch.Tensor:
@@ -110,3 +111,79 @@ class Net(nn.Module):
             if len(self.shape_param) > 2 and (i+2 < len(self.dim_param)):
                 x = f.relu(x)
         return x
+
+
+def train_model(net: nn.Module,
+                data: dict,
+                criterion: Callable,
+                n_it: int,
+                lr: float) -> Tuple[nn.Module, list, list]:
+    """Train the model (given in NetStdTraining) with Adam.
+
+    :param net: neural network to be trained
+    :param data: data set on which the network does a prediction
+    :param criterion: loss-function to train the neural network (typically: MSE)
+    :param n_it: number of iterations to train the network
+    :param lr: learning-rate
+    :return: \1) trained neural network
+             2) Losses observed during training
+             3) Corresponding iterates
+    """
+
+    # Initialize
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    iterates = []
+    losses = []
+
+    # Perform optimization (as usual)
+    for i in range(n_it + 1):
+
+        # Extract neural network (current iterate) into a single tensor
+        iterates.append(nn_to_tensor(net))
+
+        # Compute loss on the given data set and perform optimization step
+        optimizer.zero_grad()
+        loss = criterion(net(data['xes']), data['yes'])
+        losses.append(loss.item())
+        loss.backward()
+        optimizer.step()
+
+    return net, losses, iterates
+
+
+def tensor_to_nn(tensor: torch.Tensor,
+                 template: nn.Module) -> None:
+    """Transform a given tensor (of weights) into a neural network, that is, load the weights that are predicted by the
+    optimization algorithm into the neural network. This is the counterpart to nn_to_tensor().
+
+    :param tensor: tensor that contains the weights all after each other
+    :param template: template of the neural network architecture into which the weights get loaded
+    """
+
+    counter = 0
+    for param in template.parameters():
+        # If the parameter is updated by the (learned) optimization algorithm, then they have corresponding entries in
+        # the tensor, which should be loaded into the template.
+        if param.requires_grad:
+            cur_size = torch.prod(torch.tensor(param.size()))
+            cur_shape = param.shape
+            param.data = tensor[counter:counter + cur_size].reshape(cur_shape)
+            counter += cur_size
+
+
+def nn_to_tensor(neural_network: nn.Module) -> torch.Tensor:
+    """Take a neural network and extract its (learnable) weights into one big tensor. This is the counterpart to
+    tensor_to_nn().
+
+    :param neural_network: the neural network as nn.Module
+    :return: tensor that contains all the weights
+    """
+
+    # Store all parameters as flattened tensors and then concatenate them into one big tensor
+    all_params = []
+    for name, param in neural_network.named_parameters():
+        if param.requires_grad:
+            all_params.append(param.flatten())
+    x = torch.concat(all_params, dim=0).detach()
+
+    return x
