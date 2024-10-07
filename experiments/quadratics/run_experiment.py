9db818f104857ad18d2069a9206dccf1b483af49
Michael Sucker <michael_sucker@t-online.de>
2024-10-07 11:11:14 +0200

    Added implementation to 'run_experiment.py'.

--- experiments/quadratics/run_experiment.py
+++ experiments/quadratics/run_experiment.py
@@ -1,3 +1,15 @@
+from experiments.quadratics.evaluation import evaluate_quad
+from experiments.quadratics.plotting import create_plot
+
 
 def run_experiment(path: str) -> None:
-    pass
+    """Run the experiment on quadratic functions.
+
+    :param path: path where the trained model is stored, and where the data/plot get saved into
+    """
+
+    # Evaluate model
+    evaluate_quad(loading_path=path)
+
+    # Create plot
+    create_plot(loading_path=path)
