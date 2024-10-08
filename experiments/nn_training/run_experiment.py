83c14ccaf6df5f51a8ed43ad3c9007a276925155
Michael Sucker <michael_sucker@t-online.de>
2024-10-08 10:29:27 +0200

    Added print-statements for start and end of experiment.

--- experiments/nn_training/run_experiment.py
+++ experiments/nn_training/run_experiment.py
@@ -8,8 +8,12 @@ def run_experiment_nn(path: str) -> None:
     :param path: path where the trained model is stored, and where the data/plot get saved into
     """
 
+    print(f"Starting experiment 'Neural Network Training'.")
+
     # Evaluate model
     evaluate_nn(loading_path=path)
 
     # Create plot
     create_plot(loading_path=path)
+
+    print(f"End experiment 'Neural Network Training'.")
