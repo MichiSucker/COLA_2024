d8df028b9e46e1f97e3c336289d0f41ad5f5f880
Michael Sucker <michael_sucker@t-online.de>
2024-10-08 10:21:50 +0200

    Renamed function 'run_experiment()' into 'run_experiment_nn()'.

--- experiments/nn_training/run_experiment.py
+++ experiments/nn_training/run_experiment.py
@@ -2,7 +2,7 @@ from experiments.nn_training.evaluation import evaluate_nn
 from experiments.nn_training.plotting import create_plot
 
 
-def run_experiment(path: str) -> None:
+def run_experiment_nn(path: str) -> None:
     """Run the experiment on training a neural network.
 
     :param path: path where the trained model is stored, and where the data/plot get saved into
