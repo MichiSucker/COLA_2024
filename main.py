083eeb38c66d02f84abf54af11d016bd75278ec6
Michael Sucker <michael_sucker@t-online.de>
2024-10-08 10:26:47 +0200

    Added choices for which experiment should be run, as well as NotImplementedError for the remaining cases.

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-
+from experiments.quadratics.run_experiment import run_experiment_quad
+from experiments.nn_training.run_experiment import run_experiment_nn
 
 if __name__ == '__main__':
-    print("Nothing implemented yet.")
+
+    # Specify which experiment should be run. Possible choices are 'quadratics' and 'nn_training'
+    exp_to_run = 'nn_training'
+
+    # Specify the paths to the trained models, pac-bounds, etc., and where the evaluation data should be stored into
+    path_nn = '/home/michael/Desktop/AISTATS_2025/Data/nn_test/data/'
+    path_quad = '/home/michael/Desktop/AISTATS_2025/Data/quadratics_test/data/'
+
+    if exp_to_run == 'nn_training':
+        run_experiment_nn(path=path_nn)
+
+    elif exp_to_run == 'quadratics':
+        run_experiment_quad(path=path_quad)
+
+    else:
+        raise NotImplementedError(f"Experiment with name '{exp_to_run}' is not implemented.")
