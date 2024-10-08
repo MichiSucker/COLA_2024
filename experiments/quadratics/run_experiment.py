fde8998799c522ad4dcb4f04e9fedcf6ee5860f3
Michael Sucker <michael_sucker@t-online.de>
2024-10-08 10:22:06 +0200

    Renamed function 'run_experiment()' into 'run_experiment_quad()'.

--- experiments/quadratics/run_experiment.py
+++ experiments/quadratics/run_experiment.py
@@ -2,7 +2,7 @@ from experiments.quadratics.evaluation import evaluate_quad
 from experiments.quadratics.plotting import create_plot
 
 
-def run_experiment(path: str) -> None:
+def run_experiment_quad(path: str) -> None:
     """Run the experiment on quadratic functions.
 
     :param path: path where the trained model is stored, and where the data/plot get saved into
