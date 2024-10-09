226aff1c9f39d02e7a29825942fc32898fea09ee
Michael Sucker <michael_sucker@t-online.de>
2024-10-09 16:28:13 +0200

    Removed paths for anonymity.

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from experiments.nn_training.run_experiment import run_experiment_nn
 if __name__ == '__main__':
 
     # Specify which experiment should be run. Possible choices are 'quadratics' and 'nn_training'
-    exp_to_run = 'nn_training'
+    exp_to_run = 'quadratics'
 
     # Specify the paths to the trained models, pac-bounds, etc., and where the evaluation data should be stored into
-    path_nn = '/home/michael/Desktop/AISTATS_2025/Data/nn_test/data/'
-    path_quad = '/home/michael/Desktop/AISTATS_2025/Data/quadratics_test/data/'
+    path_nn = ...
+    path_quad = ...
 
     if exp_to_run == 'nn_training':
         run_experiment_nn(path=path_nn)
